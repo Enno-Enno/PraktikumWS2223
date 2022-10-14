6378aeb72556c5a7fc85c92fa2d0a3053b91b9e8
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2022-10-14 23:15:31 +0200

    Aufgabe_1 im wesentlichen fertig.
    Die Aufgabe 1 ist in einem Zustand, den man akzeptieren und abgeben
    kann. Die Sachen, die in dem latex-template Ordner standen habe ich
    nur so weit verwendet, wie ich sie verstanden habe.
    Außerdem README.md

--- Aufgabe_1/Graphik.py
+++ Aufgabe_1/Graphik.py
@@ -1,7 +1,15 @@
+import matplotlib as mpl
+mpl.use('pgf')
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit 
-
+from scipy.optimize import curve_fit
+mpl.rcParams.update({            # <-- Set matplotlib options
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+    'pgf.texsystem': 'lualatex',
+    'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}',
+})
 
 def f(x,N_0,m):
     return N_0 * np.exp(m * x)
@@ -13,8 +21,8 @@ params, covariance_matrix = curve_fit(f , d, N) # x=d und y=N
 x_lin = np.linspace(0, 5, 1000)
 
 plt.subplot(1, 2, 1)
-plt.ylabel(r'N[1/ 60 s]')
-plt.xlabel(r'd / cm ')
+plt.ylabel(r'N [1/ 60 \unit{\second}]')
+plt.xlabel(r'd / \unit{\cm} ')
 plt.plot(d, N, 'o', label='Gemessene Werte')
 
 plt.plot(x_lin,f(x_lin , params[0],params[1]), label='Theoretische Erwartung')
@@ -25,10 +33,11 @@ plt.legend(loc='best')
 plt.subplot(1, 2, 2)
 plt.plot(d, N, 'o', label='Gemessene Werte')
 plt.plot(x_lin,f(x_lin , params[0],params[1]), label='Theoretische Erwartung')
-plt.ylabel(r'N[1/ 60 s]')
-plt.xlabel(r'd / cm ')
+plt.ylabel(r'N [1/ 60 \unit{\second}]')
+plt.xlabel(r'd / \unit{\cm} ')
 plt.yscale('log')
 
 plt.legend(loc='best')
 
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/plot.pdf')
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Praktikum Winter Semester 22/23
 Im gitignore steht '\*.pdf' das heißt hier drin werden grudsätzlich keine pdfs gespeichert. Wenn wir das doof finden dann kann man das auch noch ändern. 
+Wenn man im Hauptordner make ausführt, dann werden alle Makefiles in den genannten Unterordnern aufgerufen =D
