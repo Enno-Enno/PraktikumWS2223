c441bafce95263b287a089bda79665bb38b519a5
Clara Sondermann <clara.sondermann@tu-dortmund.de>
2022-11-14 21:52:39 +0100

    Aufgabe 4: Habe den Wert (0, 0) aus der Messreihe entfernt, da es ansonten 11 Werte gewesen wären und man damit sowieso nicht rechnen konnte. Ansonsten ist die Aufgabe jetzt (hoffentlich) beendet.

--- Aufgabe4/rechnerei.py
+++ Aufgabe4/rechnerei.py
@@ -6,10 +6,15 @@ import matplotlib.pyplot as plt
 x = np.linspace(0,54)
 sum = 0
 for index in range(len(Delta_x)):
-    if index == 0: 
-        print('{--}')
-    else:
+    #if index == 0: 
+    #    print('{--}')
+    #else:
         print('{:.4f}'.format(F[index] / Delta_x[index]))
         sum += F[index] / Delta_x[index]
 print('Summe D: ', '{:.4f}'.format(sum))
 print('Mittelwert D: ', '{:.4f}'.format(sum/10))
+
+mx=np.sum(Delta_x)/len(Delta_x)
+mf=np.sum(F)/len(F)
+print('Mittelwert der Auslenkung: ', '{:.4f}'.format(mx))
+print('Mittelwert der Kraft:', '{:.4f}'.format(mf))
