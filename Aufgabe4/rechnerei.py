7540615330546c10a617e70b415b24b0761d8bcd
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2022-11-14 14:05:51 +0100

    Aufgabe 4: Sachen auslagern (Versuch)Aufgabe 4: Sachen auslagern (Versuch)

---
+++ Aufgabe4/rechnerei.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+(Delta_x, F) = np.genfromtxt('Hook_Daten.txt',unpack=True)
+
+x = np.linspace(0,54)
+sum = 0
+for index in range(len(Delta_x)):
+    if index == 0: 
+        print('{--}')
+    else:
+        print('{:.4f}'.format(F[index] / Delta_x[index]))
+        sum += F[index] / Delta_x[index]
+print('Summe D: ', '{:.4f}'.format(sum))
+print('Mittelwert D: ', '{:.4f}'.format(sum/10))
