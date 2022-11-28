bccd6239f13042ec40823b12232b9f68786d8829
Clara Sondermann <clara.sondermann@tu-dortmund.de>
2022-11-28 14:46:57 +0100

    V207: minimal bei den Messdaten weiter gemacht.

--- v207/rechnungen.py
+++ v207/rechnungen.py
@@ -5,16 +5,24 @@ import matplotlib.pyplot as plt
 (gross_oben, gross_unten) = np.genfromtxt("Messdaten_grKu_Zitemp.txt", unpack=True)
 (temp, temp_oben, temp_unten) = np.genfromtxt('Messdaten_grKu_steigendeTemp.txt', unpack=True)
 
-t_oben_klein = np.mean(klein_oben)
-t_unten_klein = np.mean(klein_unten)
+mw_oben_klein = np.mean(klein_oben)
+st_oben_klein = np.std(klein_oben)
 
-t_oben_gross = np.mean(gross_oben)
-t_unten_gross = np.mean(gross_unten)
+mw_unten_klein = np.mean(klein_unten)
+st_unten_klein = np.std(klein_unten)
+
+mw_oben_gross = np.mean(gross_oben)
+st_oben_gross = np.std(gross_oben)
+
+mw_unten_gross = np.mean(gross_unten)
+st_unten_gross = np.std(gross_unten)
+
+
 
 print("Durchschnittszeiten kleine Kugel:")
-print("oben:", '{:.5f}'.format(t_oben_klein))
-print("unten:", '{:.5f}'.format(t_unten_klein))
+print("oben:", '{:.5f}'.format(mw_oben_klein), "mit Fehler: ", "{:.5f}".format(st_oben_klein))
+print("unten:", '{:.5f}'.format(mw_unten_klein), "mit Fehler: ", "{:.5f}".format(st_unten_klein))
 
-print("Durchschnittszeiten grosse Kugel:")
-print("oben:", '{:.5f}'.format(t_oben_gross))
-print("unten:", '{:.5f}'.format(t_unten_gross))
+print("Durchschnittszeiten gross Kugel:")
+print("oben:", '{:.5f}'.format(mw_oben_gross), "mit Fehler: ", "{:.5f}".format(st_oben_gross))
+print("unten:", '{:.5f}'.format(mw_unten_gross), "mit Fehler: ", "{:.5f}".format(st_unten_gross))
