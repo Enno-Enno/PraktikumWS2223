072f0b476de5c30b2b1589dcad6976c13c312601
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2022-11-28 23:47:33 +0100

    v207: Zwischenstand schnelles nicht plotten und essen

--- v207/plot.py
+++ v207/plot.py
@@ -36,16 +36,41 @@ unc_t_unten = unp.uarray(artm_t_unten, sdev_t_unten)
 
 
 
-plt.errorbar(single_temp, artm_t_oben, yerr=sdev_t_oben, fmt='o', label=r'Laufzeiten oben')
-plt.errorbar(single_temp, artm_t_unten, yerr=sdev_t_unten, fmt='o', label=r'Laufzeiten unten')
-
-plt.ylabel("t / \\unit{{\\s}}")
-plt.xlabel("T / \\unit{{\\celsius}}")
-plt.savefig("build/Messreihe3.pdf")
-
-
-#rho_temp, rho_Wasser = np.genfromtxt('Geschke_Wasserdichte.txt',unpack = True)
+#plt.errorbar(single_temp, artm_t_oben, yerr=sdev_t_oben, fmt='o', label=r'Laufzeiten oben')
+#plt.errorbar(single_temp, artm_t_unten, yerr=sdev_t_unten, fmt='o', label=r'Laufzeiten unten')
 #
-#dichten = dict()
-#for index in enumerate(rho_temp)
+#plt.ylabel("t / \\unit{{\\s}}")
+#plt.xlabel("T / \\unit{{\\celsius}}")
+#plt.savefig("build/Messreihe3.pdf")
 
+
+rho_temp, rho_Wasser = np.genfromtxt('Geschke_Wasserdichte.txt',unpack = True, dtype=None)
+print('rho_temp',rho_temp)
+print('rho_Wasser',rho_Wasser)
+
+dichten = dict()
+for index in range(len(rho_temp)):
+    dichten[rho_temp[index]]= rho_Wasser[index] 
+
+print(dichten)
+
+def eta(K, rho_Kugel, rho_Wasser, t):
+    return K * (rho_Kugel - rho_Wasser) * t
+
+
+#Messreihe 1:
+#Durchschnittszeiten kleine Kugel:
+#oben: 12.32400 mit Fehler:  0.15500
+#unten: 12.24800 mit Fehler:  0.18405
+#Messreihe 2:
+#Durchschnittszeiten grosse Kugel:
+#oben: 35.02400 mit Fehler:  0.29574
+#unten: 35.60000 mit Fehler:  0.75913
+#Reynold:
+#Geschwindigkeit klein oben:  0.811+/-0.010
+#Geschwindigkeit klein unten:  0.816+/-0.012
+#Geschwindigkeit gross oben:  0.1428+/-0.0012
+#Geschwindigkeit gross unten:  0.1404+/-0.0030
+#eta empirisch klein:  0.001152+/-0.000018
+#K oben und gross:  (2.33+/-0.04)e-05
+#K unten und gross:  (2.30+/-0.06)e-05
