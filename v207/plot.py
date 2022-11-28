93ab11e6fc386d0db7b5dc8daa37414dcf05257b
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2022-11-28 21:45:58 +0100

    v207: Kosmetische Sachen und Nerven behalten

--- v207/plot.py
+++ v207/plot.py
@@ -1,21 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-temp, temp_oben, temp_unten = np.genfromtxt("Messdaten_grKu_steigendeTemp.txt", unpack=True)
+temp, t_oben, t_unten = np.genfromtxt("Messdaten_grKu_steigendeTemp.txt", unpack=True)
 n=10
-y_oben=np.zeros(n)
-y_unten=np.zeros(n)
+artm_t_oben  =np.zeros(n)
+artm_t_unten =np.zeros(n)
+sdev_t_oben  =np.zeros(n)
+sdev_t_unten =np.zeros(n)
 
 for j in range(n):
     for i, temperature in enumerate(temp):
 
         if i==2*j and temp[i] == temp[i+1]:
-            y_oben[j] = (temp_oben[i] + temp_oben[i+1])/2
-            y_unten[j] = (temp_unten[i] + temp_unten[i+1])/2
+            artm_t_oben[j] = (t_oben[i] + t_oben[i+1])/2
+            artm_t_unten[j] = (t_unten[i] + t_unten[i+1])/2
+            sdev_t_oben[j] = np.std(t_oben[j:j+2])
+            sdev_t_unten[j] = np.std(t_unten[j:j+2])
 
-#print(y_oben)
+# Hier werden paarweise die Mittewerte von den "oben" und den "unten" Messwerten berechnet
 
-plt.plot(temp, np.exp(1/(np.exp(temp))), 'b.')
+print("artm_t_oben",artm_t_oben)
+print("artm_t_unten",artm_t_oben)
+
+
+
 plt.yscale('log')
-
-plt.show()
