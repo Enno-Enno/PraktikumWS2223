b38311852737c39b6b034185fd96e7d3e4aa57c3
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2022-11-28 22:25:03 +0100

    v207: Änderungen an dem Plot für Messreihe 3

--- v207/plot.py
+++ v207/plot.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
 
 temp, t_oben, t_unten = np.genfromtxt("Messdaten_grKu_steigendeTemp.txt", unpack=True)
 n=10
@@ -7,6 +9,7 @@ artm_t_oben  =np.zeros(n)
 artm_t_unten =np.zeros(n)
 sdev_t_oben  =np.zeros(n)
 sdev_t_unten =np.zeros(n)
+single_temp  =np.zeros(n)
 
 for j in range(n):
     for i, temperature in enumerate(temp):
@@ -16,12 +19,23 @@ for j in range(n):
             artm_t_unten[j] = (t_unten[i] + t_unten[i+1])/2
             sdev_t_oben[j] = np.std(t_oben[j:j+2])
             sdev_t_unten[j] = np.std(t_unten[j:j+2])
+            single_temp[j]= temp[i]
 
 # Hier werden paarweise die Mittewerte von den "oben" und den "unten" Messwerten berechnet
 
-print("artm_t_oben",artm_t_oben)
-print("artm_t_unten",artm_t_oben)
+print("artm_t_oben ",artm_t_oben)
+print("artm_t_unten",artm_t_unten)
+print("sdev_t_oben ",sdev_t_oben)
+print("sdev_t_unten",sdev_t_unten)
+print("single_temp",single_temp)
 
+#np.savetxt('Messreihe3.txt', np.column_stack([artm_t_oben, artm_t_unten, sdev_t_oben, sdev_t_unten]), header="artm_t_oben artm_t_unten sdev_t_oben sdev_t_unten")
 
+unc_t_oben = unp.uarray(artm_t_oben, sdev_t_oben)
+unc_t_unten = unp.uarray(artm_t_unten, sdev_t_unten)
+
+plt.errorbar(single_temp, artm_t_oben, yerr=sdev_t_oben, fmt='o')
 
 plt.yscale('log')
+
+plt.savefig("build/Messreihe3.pdf")
