73d10855fe238a61135bfb9c657dd24275ebeae4
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2022-11-28 19:26:27 +0100

    v 207: Dichte der Kugeln
    muss man halt sehen...

--- v207/rechnungen.py
+++ v207/rechnungen.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from uncertainties import ufloat
 
 (klein_oben, klein_unten) = np.genfromtxt("Messdaten_klKu_Zitemp.txt", unpack=True)
 (gross_oben, gross_unten) = np.genfromtxt("Messdaten_grKu_Zitemp.txt", unpack=True)
@@ -23,6 +24,16 @@ print("Durchschnittszeiten kleine Kugel:")
 print("oben:", '{:.5f}'.format(mw_oben_klein), "mit Fehler: ", "{:.5f}".format(st_oben_klein))
 print("unten:", '{:.5f}'.format(mw_unten_klein), "mit Fehler: ", "{:.5f}".format(st_unten_klein))
 
-print("Durchschnittszeiten gross Kugel:")
+print("Durchschnittszeiten grosse Kugel:")
 print("oben:", '{:.5f}'.format(mw_oben_gross), "mit Fehler: ", "{:.5f}".format(st_oben_gross))
 print("unten:", '{:.5f}'.format(mw_unten_gross), "mit Fehler: ", "{:.5f}".format(st_unten_gross))
+
+
+drcm_klKu = ufloat(15.11, 0.01)
+drcm_grKu = ufloat(15.78, 0.01)
+m_gr = ufloat(4.9528, 0)
+m_kl = ufloat(4.4531, 0)
+rho_gr = ((3 * m_gr)/(4 * np.pi))* (2/drcm_grKu)**3
+rho_kl = ((3 * m_kl)/(4 * np.pi))* (2/drcm_klKu)**3
+print("Dichte große Kugel uncertainties",rho_gr)
+print("Dichte kleine Kugel uncertainties",rho_kl)
