16de459319604775b34ca9883923b1a7e0ec55b1
Clara Sondermann <clara.sondermann@tu-dortmund.de>
2022-12-02 16:00:44 +0100

    v206: viele plots erstellt und in auswertung eingebunden aber die parameter von 2 und 3 passen noch nicht. makefile auch überarbeitet.

---
+++ v206/plot_ausgleich_3.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+t, temp_k, durck_k, temp_w, durck_w, leistung = np.genfromtxt("messdaten.txt", unpack=True)
+
+def function_3(t, a, b, c, alpha) :
+    return ((a*t**alpha)/(1 + b*t**alpha) + c)
+
+params_k, covariance_matrix_k = curve_fit(function_3, t, temp_k, p0=(1, 1, 1, 1.5))
+params_w, covariance_matrix_w = curve_fit(function_3, t, temp_w, p0=(1, 1, 1, 1.5))
+#standardwerte anpassen!
+
+print("Funktion 3 für kalt:")
+for name, value in zip('abcp', params_k):
+    print(f"{name} = {value:8.8f}")
+
+print("Funktion 3 für warm:")
+for name, value in zip('abcp', params_w):
+    print(f"{name} = {value:8.8f}")
+
+x=np.linspace(0, 25, 1000)
+plt.plot(t, temp_k, "x", label="$T_{{\\text{k}}}$")
+plt.plot(t, temp_w, "x", label="$T_{{\\text{w}}}$")
+plt.plot(x, function_3(x, *params_k), "-", label = "Funktion 3 für $T_{{\\text{k}}}$")
+plt.plot(x, function_3(x, *params_w), "-", label = "Funktion 3 für $T_{{\\text{w}}}$")
+plt.legend()
+plt.xlabel("$t / \\unit{{\\minute}}$")
+plt.ylabel("$T/ \\unit{{\\celsius}}$")
+plt.savefig("build/plot_ausgleich_3.pdf")
