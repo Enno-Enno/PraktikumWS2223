4cf879bb79cfe1f0e6d3e50587b5d426e3a76719
Clara Sondermann <clara.sondermann@tu-dortmund.de>
2022-12-02 21:22:56 +0100

    v206: ausgleichsplots und deren auswertung beendet

--- 02_v206/plot_ausgleich_3.py
+++ 02_v206/plot_ausgleich_3.py
@@ -7,9 +7,19 @@ t, temp_k, durck_k, temp_w, durck_w, leistung = np.genfromtxt("messdaten.txt", u
 def function_3(t, a, b, c, alpha) :
     return ((a*t**alpha)/(1 + b*t**alpha) + c)
 
-params_k, covariance_matrix_k = curve_fit(function_3, t, temp_k, p0=(1, 1, 1, 1.5))
-params_w, covariance_matrix_w = curve_fit(function_3, t, temp_w, p0=(1, 1, 1, 1.5))
-#standardwerte anpassen!
+params_k, covariance_matrix_k = curve_fit(function_3, t, temp_k, p0=(-1, 0, 21, 1))
+params_w, covariance_matrix_w = curve_fit(function_3, t, temp_w, p0=(1, 0, 21, 1))
+
+#Funktion 3 für kalt:
+#a = -0.91132029
+#b = 0.02042680
+#c = 21.51604450
+#p = 1.19462353
+#Funktion 3 für warm:
+#a = 1.13913878
+#b = 0.02290029
+#c = 20.89673747
+#p = 1.22110022
 
 print("Funktion 3 für kalt:")
 for name, value in zip('abcp', params_k):
@@ -26,5 +36,5 @@ plt.plot(x, function_3(x, *params_k), "-", label = "Funktion 3 für $T_{{\\text{
 plt.plot(x, function_3(x, *params_w), "-", label = "Funktion 3 für $T_{{\\text{w}}}$")
 plt.legend()
 plt.xlabel("$t / \\unit{{\\minute}}$")
-plt.ylabel("$T/ \\unit{{\\celsius}}$")
+plt.ylabel("$T/ \\unit{{\\celsius}}$ test")
 plt.savefig("build/plot_ausgleich_3.pdf")
