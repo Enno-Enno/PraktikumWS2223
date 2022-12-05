151974628f3adc6323815089f96ca94d41dc4ada
Clara Sondermann <clara.sondermann@tu-dortmund.de>
2022-12-05 15:32:10 +0100

    v206: rechnungen für die mechanische arbeit angefangen

--- 02_v206/fehler.py
+++ 02_v206/fehler.py
@@ -103,7 +103,23 @@ print("massendurchsatz 3: ", massendurchsatz_3)
 
 #rechnungen für mechanische Leistung N_mech:
 
-#dm/dt in g/min
+import numpy as np
+import matplotlib.pyplot as plt
+
+t, temp_k, druck_k, temp_w, druck_w, leistung = np.genfromtxt("messdaten.txt", unpack=True)
+
+for i,value in enumerate(temp_k):
+    temp_k[i] += 273.15
+#in K
+
+for i,value in enumerate(druck_k):
+    druck_k[i] *= 100000
+#in Pa
+
+for i,value in enumerate(druck_k):
+    druck_w[i] *= 100000
+#in Pa
+
 
 rho_0 = ufloat(5.51, 0)
 #in g/L = g/(dm)^3 = 10^3 g/m^3
@@ -111,6 +127,40 @@ rho_0 = ufloat(5.51, 0)
 rho_0 *= 10**3
 #in g/m^3
 
-rho = 
+T_0 = ufloat(273.15, 0) # in K
 
-kappa = 1.14
+p_0 = ufloat(100000, 0) # in Pa
+
+rho_k = rho_0 * T_0 * druck_k/(temp_k * p_0) # in g/m^3
+
+print("rho: ", rho_k)
+#rho:  
+# [24543.13300492611+/-0 20466.517083120856+/-0 22605.388632872502+/-0
+# 22713.937918024356+/-0 23342.25142167844+/-0 23439.191036511504+/-0
+# 22997.911442958848+/-0 21504.553580763193+/-0 21060.787126115094+/-0
+# 21134.72353870458+/-0 20141.62599049128+/-0 20212.810390528357+/-0
+# 19210.081191278143+/-0 18736.253779121467+/-0 18259.383050847457+/-0
+# 17773.077294685987+/-0 17824.103534900412+/-0 17327.507825148405+/-0
+# 16834.476456792352+/-0 16332.680412371132+/-0 16374.141432456934+/-0
+# 15862.852444121389+/-0 15349.328719723184+/-0 15382.946523088152+/-0
+# 15411.07405375754+/-0 15444.96316657504+/-0]
+
+#dm/dt in g/min
+
+#p_a = p_k, p_b = p_w, T_2 = T_k
+
+kappa = ufloat(1.14, 0)
+
+#test = (rho_0)**(1/kappa)
+#print(test)
+
+n_mech_3 = 1/(kappa - 1) * (druck_w[3] * (druck_k[3]/druck_w[3])**(1/kappa) - druck_k[3]) * 1/rho_k[3] * massendurchsatz_0
+n_mech_9 = 1/(kappa - 1) * (druck_w[9] * (druck_k[9]/druck_w[9])**(1/kappa) - druck_k[9]) * 1/rho_k[9] * massendurchsatz_1
+n_mech_15 = 1/(kappa - 1) * (druck_w[15] * (druck_k[15]/druck_w[15])**(1/kappa) - druck_k[15]) * 1/rho_k[15] * massendurchsatz_2
+n_mech_21 = 1/(kappa - 1) * (druck_w[21] * (druck_k[21]/druck_w[21])**(1/kappa) - druck_k[21]) * 1/rho_k[21] * massendurchsatz_3
+#n_mech in 1 * Pa * m^3/g * g/min = Pa * m^3 / min = kg /(m* s^2) * m^3 /min = kg* m^2 /(s^2 * min)
+
+print("mechanische leistung nach 3s:", n_mech_3)
+n_mech_3 /= 60
+
+print("mechanische leistung nach 3s:", n_mech_3)
