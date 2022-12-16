6234bd77e62822cb62b30e3a254fdcf38a987f0b
Clara Sondermann <clara.sondermann@tu-dortmund.de>
2022-12-16 17:41:08 +0100

    v206: abschnitt messdaten auf auswertung und durchführung aufgeteilt und korrekturen.

--- 02_v206/plot_temp_verlauf.py
+++ 02_v206/plot_temp_verlauf.py
@@ -11,6 +11,7 @@ for i,value in enumerate(temp_k):
 plt.figure()
 plt.plot(t, temp_k, "x", label="$T_{{\\text{k}}}$")
 plt.plot(t, temp_w, "x", label="$T_{{\\text{w}}}$")
+plt.grid()
 plt.legend()
 plt.xlabel("$t / \\unit{{\\minute}}$")
 plt.ylabel("$T/ \\unit{{\\kelvin}}$")
