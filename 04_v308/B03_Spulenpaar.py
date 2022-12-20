c64567d8bf123a43ef4df4d99c960b5d32def5bb
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2022-12-20 17:34:30 +0100

    v308: letzter commit
    Diskussion gemacht. Und das Spulenpaar.

--- 04_v308/B03_Spulenpaar.py
+++ 04_v308/B03_Spulenpaar.py
@@ -12,6 +12,9 @@ mw_6 = np.mean(magnetfeld_6[:4])* np.ones(4)
 mw_12 = np.mean(magnetfeld_12[7:])* np.ones(7)
 mw_24 = np.mean(magnetfeld_24[:13])* np.ones(13)
 
+mw_a_6 = np.mean(magnetfeld_6[4:]   ) * np.ones(len(magnetfeld_6[4:]))
+mw_a_12 = np.mean(magnetfeld_12[:7] ) * np.ones(7)
+mw_a_24 = np.mean(magnetfeld_24[13:]) * np.ones(2)
 
 
 
@@ -20,8 +23,10 @@ print("Hier entsteht der erste Plot")
 fig, (ax6, ax12, ax24) = plt.subplots(3,1, constrained_layout=True, sharey=True, figsize=(7,9))
 
 #plt.subplot(3,1,1) # Magnetfeld innerhalb der Spulen 6 cm
-ax6.plot(x_6[:4], magnetfeld_6[:4], 'x', label="Magnetfeldstärke")
-ax6.plot(x_6[:4], mw_6, label="$\\overline{{B}}$")
+ax6.plot(x_6[:4], magnetfeld_6[:4], 'x', label="Magnetfeldstärke innen")
+ax6.plot(x_6[:4], mw_6, label="$\\overline{{B_i}}$")
+ax6.plot(x_6[4:], magnetfeld_6[4:], 'x', label="Magnetfeldstärke außen")
+ax6.plot(x_6[4:], mw_a_6, label="$\\overline{{B_a}}$")
 ax6.grid()
 ax6.set_title("$\\qty{{6}}{{cm}}$ Spulenabstand")
 ax6.set_xlabel("$x/ \\unit{{cm}}$")
@@ -30,8 +35,10 @@ ax6.legend()
 
 #plt.subplot(3,1,2) # Magnetfeld innerhalb der Spulen 12 cm
 ax12.grid()
-ax12.plot(x_12[7:], magnetfeld_12[7:], 'x', label="Magnetfeldstärke")
-ax12.plot(x_12[7:], mw_12, label="$\\overline{{B}}$")
+ax12.plot(x_12[7:], magnetfeld_12[7:], 'x', label="Magnetfeldstärke innen")
+ax12.plot(x_12[7:], mw_12, label="$\\overline{{B_i}}$")
+ax12.plot(x_12[:7], magnetfeld_12[:7], 'x', label="Magnetfeldstärke außen")
+ax12.plot(x_12[:7], mw_a_12, label="$\\overline{{B_a}}$")
 ax12.set_title("$\\qty{{12}}{{cm}}$ Spulenabstand")
 ax12.set_xlabel("$x/ \\unit{{cm}}$")
 ax12.set_ylabel("B/ \\unit{{\\milli\\tesla}}")
@@ -39,8 +46,10 @@ ax12.legend()
 
 
 #plt.subplot(2,1,1) # Magnetfeld innerhalb der Spulen 24 cm
-ax24.plot(x_24[:13], magnetfeld_24[:13], 'x', label="Magnetfeldstärke")
-ax24.plot(x_24[:13], mw_24, label="$\\overline{{B}}$")
+ax24.plot(x_24[:13], magnetfeld_24[:13], 'x', label="Magnetfeldstärke innen")
+ax24.plot(x_24[:13], mw_24, label="$\\overline{{B_i}}$")
+ax24.plot(x_24[13:], magnetfeld_24[13:], 'x', label="Magnetfeldstärke außen")
+ax24.plot(x_24[13:], mw_a_24, label="$\\overline{{B_a}}$")
 ax24.grid()
 
 ax24.set_title("$\\qty{24}{{\\centi\\meter}}$ Spulenabstand")
@@ -52,33 +61,34 @@ ax24.legend()
 plt.savefig("build/B03_innen_Spulenpaar.pdf")
 
 #####
-fig, (ax6, ax12, ax24) = plt.subplots(3,1, constrained_layout=True, sharey=True, figsize=(7,9))
-print("Hier passiert gerade der zweite Plot")
-
-#plt.subplot(3,1,1) # Magnetfeld innerhalb der Spulen 6 cm
-ax6.plot(x_6[4:], magnetfeld_6[4:], 'x', label="Magnetfeld außerhalb der Spulen")
-ax6.grid()
-ax6.set_title("$\\qty{{6}}{{cm}}$ Spulenabstand")
-ax6.set_xlabel("$x/ \\unit{{cm}}$")
-ax6.set_ylabel("B/ \\unit{{\\milli\\tesla}}")
-
-#plt.subplot(3,1,2) # Magnetfeld außerhalb der Spulen 12 cm
-ax12.grid()
-ax12.plot(x_12[:7], magnetfeld_12[:7], 'x' )
-ax12.set_title("$\\qty{{12}}{{cm}}$ Spulenabstand")
-ax12.set_xlabel("$x/ \\unit{{cm}}$")
-ax12.set_ylabel("B/ \\unit{{\\milli\\tesla}}")
-
-
-#plt.subplot(2,1,1) # Magnetfeld innerhalb der Spulen 24 cm
-ax24.plot(x_24[13:], magnetfeld_24[13:], 'x', label="Magnetfeld innerhalb der Spulen")
-ax24.grid()
-
-ax24.set_title("$\\qty{24}{{\\centi\\meter}}$ Spulenabstand")
-ax24.set_xlabel("$x/ \\unit{{cm}}$")
-ax24.set_ylabel("B/ \\unit{{\\milli\\tesla}}")
-
-
-plt.savefig("build/B03_aussen_Spulenpaar.pdf")
-
-
+#fig, (ax6, ax12, ax24) = plt.subplots(3,1, constrained_layout=True, sharey=True, figsize=(7,9))
+#print("Hier passiert gerade der zweite Plot")
+#
+##plt.subplot(3,1,1) # Magnetfeld innerhalb der Spulen 6 cm
+#ax6.plot(x_6[4:], magnetfeld_6[4:], 'x', label="Magnetfeld außerhalb der Spulen")
+#ax6.grid()
+#ax6.set_title("$\\qty{{6}}{{cm}}$ Spulenabstand")
+#ax6.set_xlabel("$x/ \\unit{{cm}}$")
+#ax6.set_ylabel("B/ \\unit{{\\milli\\tesla}}")
+#
+##plt.subplot(3,1,2) # Magnetfeld außerhalb der Spulen 12 cm
+#ax12.grid()
+#ax12.plot(x_12[:7], magnetfeld_12[:7], 'x' )
+#ax12.set_title("$\\qty{{12}}{{cm}}$ Spulenabstand")
+#ax12.set_xlabel("$x/ \\unit{{cm}}$")
+#ax12.set_ylabel("B/ \\unit{{\\milli\\tesla}}")
+#
+#
+##plt.subplot(2,1,1) # Magnetfeld innerhalb der Spulen 24 cm
+#ax24.plot(x_24[13:], magnetfeld_24[13:], 'x', label="Magnetfeld innerhalb der Spulen")
+#ax24.grid()
+#
+#ax24.set_title("$\\qty{24}{{\\centi\\meter}}$ Spulenabstand")
+#ax24.set_xlabel("$x/ \\unit{{cm}}$")
+#ax24.set_ylabel("B/ \\unit{{\\milli\\tesla}}")
+#
+#
+#plt.savefig("build/B03_aussen_Spulenpaar.pdf")
+#
+#
+#
