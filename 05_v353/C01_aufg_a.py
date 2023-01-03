be864aa17a535801ac16d9c239b9502d7116e205
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2023-01-04 00:05:25 +0100

    v353: Plot sieht immer noch seltsam aus.
    aber ich mache mir nicht mehr so viele Sorgen da drum...

--- 05_v353/C01_aufg_a.py
+++ 05_v353/C01_aufg_a.py
@@ -15,15 +15,15 @@ t = t_k * 2  # t in ms
 #def U_fit(t, U_0, U_inf, RC):
 #    return U_0 * np.exp(-t/ RC) + U_inf
 
-U_inf = 3.8
+U_inf = 4 * 0.2 
 params, cov_matrix = np.polyfit(t , np.log(U + U_inf),deg=1, cov=True)
 U_pos = np.log(U + U_inf)
-#uncertainties = np.sqrt(np.diag(cov_matrix))
+abweichung = np.sqrt(np.diag(cov_matrix))
 
-#for name, value, uncertainty in zip('abc', params, uncertainties): 
-#    print(f'{name} = {value:8.3f} ± {uncertainty:.3f}')
+
 print("params", params)
 print("cov_matrix:", cov_matrix)
+print("abweichung:", abweichung)
 
 print("Hier entsteht Plot 1")
 fig, (ax1,ax2) = plt.subplots(2,1,constrained_layout=True, sharex=True)
@@ -37,9 +37,9 @@ ax1.set_ylabel("$U / \\unit{{\\volt}}$")
 ax1.grid()
 
 ax2.plot(t, U_pos, 'x')
-ax2.plot(x_plot, params[0]*x_plot+ params[1]*x_plot)
+ax2.plot(x_plot, params[0]*x_plot+ params[1])
 ax2.set_xlabel("$t / \\unit{{\\milli\\s}}$")
-ax2.set_ylabel("$\ln(U / U_{{\\inf}})$")
+ax2.set_ylabel("$\ln\\left(U / U\\left(\\infty \\right)\\right)$")
 ax2.grid()
 
 plt.savefig("build/C01_aufg_a.pdf")
