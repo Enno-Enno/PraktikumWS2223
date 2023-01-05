4db300cd7394bbe8b066da3be50dcb5a30df7df9
Enno Wellmann <enno.wellmann@tu-dortmund.de>
2023-01-05 13:28:51 +0100

    V353: Labels und Tabellen hinzugefügt
    Bei C01 weitergeschrieben

--- 05_v353/C01_aufg_a.py
+++ 05_v353/C01_aufg_a.py
@@ -10,12 +10,16 @@ U_k, t_k = np. genfromtxt("messdaten/A/a_in_kaestchen.txt", unpack=True)
 U = U_k * 0.2 # U in Volt
 t = t_k * 2  # t in ms
 
+for index,  zeit in enumerate(t):
+    print(zeit, U[index])
+
 #U_ln = np.log(abs(U))
 
 #def U_fit(t, U_0, U_inf, RC):
 #    return U_0 * np.exp(-t/ RC) + U_inf
 
-U_inf = 4 * 0.2 
+
+U_inf = 3.81 * 0.2 
 params, cov_matrix = np.polyfit(t , np.log(U + U_inf),deg=1, cov=True)
 U_pos = np.log(U + U_inf)
 abweichung = np.sqrt(np.diag(cov_matrix))
@@ -29,8 +33,9 @@ print("Hier entsteht Plot 1")
 fig, (ax1,ax2) = plt.subplots(2,1,constrained_layout=True, sharex=True)
 
 x_plot = np.linspace(0, 10)
-ax1.plot(t, U,'x', label="U(t)")
+ax1.plot(t, U + U_inf,'x', label="U(t)")
 #ax1.plot(x_plot, U_fit(x_plot, *params ))
+#ax1.set_yscale("log")
 ax1.set_xlabel("$t / \\unit{{\\milli\\s}}$")
 ax1.set_ylabel("$U / \\unit{{\\volt}}$")
 
