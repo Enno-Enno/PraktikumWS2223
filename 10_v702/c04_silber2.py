99d87a349a00a697b207d8ea978b24318651a895
Clara Sondermann <clara.sondermann@tu-dortmund.de>
2023-04-24 20:45:14 +0200

    v702: auswertung done

--- 10_v702/c04_silber2.py
+++ 10_v702/c04_silber2.py
@@ -5,6 +5,24 @@ from uncertainties import unumpy as unp
 import c01_nulleffekt as c01
 from scipy.optimize import curve_fit
 
+# parameter langlebig:
+# a = -0.02346305 ± 0.01272034
+# b = 3.13246545 ± 0.47801445
+# lamda_l in 1/s:  0.0023+/-0.0013 /s
+# halbwertszeit_l in sekunden:  (3.0+/-1.6)e+02 s
+# halbwertszeit_l in minuten:  4.9+/-2.7 min
+# const_l = N(0)*(1-exp(...)) =  23+/-11
+
+# parameter kurzlebig:
+# a = -0.24937942 ± 0.01808827
+# b = 5.09844481 ± 0.05853057
+# lamda_k in 1/s:  0.0249+/-0.0018 /s
+# halbwertszeit_k in sekunden:  27.8+/-2.0 s
+# halbwertszeit_k in minuten:  0.463+/-0.034 min
+# const_k = N(0)*(1-exp(...)) =  164+/-10
+# N_kurz =  0.09+/-0.05
+# N_lang =  11+/-7
+# N_abw =  0.992+/-0.007
 
 x, count_ag = np.genfromtxt("messung_c04_silber2.txt", unpack = True)
 delta_t = 10
@@ -19,43 +37,33 @@ count_ag = np.delete(count_ag, 49)
 count_ag = np.delete(count_ag, 47)
 count_ag = np.delete(count_ag, 44)
 
-#for index, value in enumerate(count_ag):
-#    print(x[index], ": ", value)
 
 t_star = 30
 # literatur lange halbwertszeit ca 2.3 minuten
-#29: halbwertszeit_l in minuten:  3.4+/-0.9 min
-#   tmax = 7 halbwertszeit_k in sekunden:  23.7+/-0.8 s
-#           9 halbwertszeit_k in sekunden:  24.1+/-0.8 s
-#35: halbwertszeit_l in minuten:  2.3+/-0.9 min (Das ist sexy aber tmax passt nicht)
-#   tmax = 7 halbwertszeit_k in sekunden:  17.5+/-0.6 s
-#           9 halbwertszeit_k in sekunden:  17.2+/-0.7 s
-#25: halbwertszeit_l in minuten:  5.1+/-2.2 min ---> näääääääääää
-#   tmax = 7 halbwertszeit_k in sekunden:  26.1+/-1.0 s
-#           10 halbwertszeit_k in sekunden:  26.8+/-0.9 s
-#40: halbwertszeit_l in minuten:  2.6+/-2.0 min
-#       tmax = 9 halbwertszeit_k in sekunden:  20.2+/-0.7 s
-#               11 halbwertszeit_k in sekunden:  20.8+/-0.9 s
-#                7 halbwertszeit_k in sekunden:  20.3+/-0.6 s
-#                   4 halbwertszeit_k in sekunden:  20.5+/-1.2 s  --> imma go with this one oder so
+# 28 halbwertszeit_l in minuten:  7+/-5 min
+# 29 halbwertszeit_l in minuten:  5.6+/-3.3 min
+# 30 halbwertszeit_l in minuten:  4.9+/-2.7 min
+# 35 halbwertszeit_l in minuten:  -6+/-6 min              NEIN 33, 34, 35, 36, 37, 38, 39, ...
+# 32 halbwertszeit_l in minuten:  11+/-17 min            mean- std wird negativ 32, 31
 
-#TMAX MUSS NOCH ANGEPASST WERDEN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-t_max  = 4 # ab 10 werden differenzen der beiden Zeiten negativ -> max 9
+
+t_max  = 12 # ab 13 werden differenzen der beiden Zeiten negativ -> max 12, nehmen wa
 # literaturwert kurze halbwertszeit ca. 24,56
-# 9: halbwertszeit_k in sekunden:  17.2+/-0.8 s
-# 8: halbwertszeit_k in sekunden:  17.3+/-0.8 s
-# 7: halbwertszeit_k in sekunden:  17.5+/-0.8 s
-# 6: halbwertszeit_k in sekunden:  17.5+/-0.8 s
-# 5: halbwertszeit_k in sekunden:  17.4+/-1.1 s
-# 4: halbwertszeit_k in sekunden:  18.5+/-1.3 s
-# 3: halbwertszeit_k in sekunden:  20.370354444849557+/-inf s -> das sollten wir lassen
+# 12 halbwertszeit_k in sekunden:  27.8+/-2.0 s
+# 11 halbwertszeit_k in sekunden:  27.8+/-2.1 s
+# 10 halbwertszeit_k in sekunden:  28.0+/-2.4 s
+# 9 halbwertszeit_k in sekunden:  27.9+/-2.7 s
+# 8 halbwertszeit_k in sekunden:  27.9+/-3.3 s
+# 7 halbwertszeit_k in sekunden:  28+/-4 s
+# 6 halbwertszeit_k in sekunden:  28+/-6 s
+# 5 halbwertszeit_k in sekunden:  29+/-9 s
+# 4 halbwertszeit_k in sekunden:  35+/-20 s
 
 # zerfall nach abzug des nulleffekts je 10s
 zerfall_mean = count_ag - c01.mean_silber
 zerfall_std = c01.std_silber #vgl. one-note bzw gaußsche fehlerfortpflanzung
 
 array = unp.uarray(zerfall_mean, zerfall_std * np.ones(len(x))) #array = N(t) (Zerfallsanzahls)
-print("array: ", array)
 array_log = unp.log(array)
 
 def lin(t, a, b):
@@ -152,8 +160,8 @@ yerr_max = y_max - unp.log(zerfall_mean)
 
 x0 = 1
 x1 = 50
-y0 = 0.9
-y1 = 5.6
+y0 = -0.2
+y1 = 5.5
 xplot = np.linspace(x0, x1, 1000)
 xplot_l = np.linspace(t_star, x1, 1000)
 xplot_k = np.linspace(x0, t_max, 1000)
@@ -164,14 +172,10 @@ plt.errorbar(x, unp.log(zerfall_mean), yerr = (yerr_min, yerr_max), fmt = "x", l
 plt.plot(xplot, params_l[0] * xplot + params_l[1], label = "Ausgleichsgerade (langlebig)")
 plt.plot(xplot, params_k[0] * xplot + params_k[1], label = "Ausgleichsgerade (kurzlebig)")
 plt.plot(xplot, unp.log(unp.exp((params_l[0]*xplot + params_l[1])) + unp.exp((params_k[0]*xplot + params_k[1]))), label='Summe beider Ausgleichsgeraden')
-# plt.plot(xplot, params_l[0] * xplot + params_l[1],"r--")
-# plt.plot(xplot_l, params_l[0] * xplot_l + params_l[1],"r-", label = "Ausgleichsgerade (langlebig) ab $t^*$")
-# plt.plot(xplot, params_k[0] * xplot + params_k[1],"g--")
-# plt.plot(xplot_k, params_k[0] * xplot_k + params_k[1],"g-", label = "Ausgleichsgerade (kurzlebig) bis $t_\\text{max}$")
 plt.xlim(x0-1, x1+1)
 plt.ylim(y0, y1)
 plt.xlabel("Zeitintervall / $\\qty{10}{\\second}$")
-plt.ylabel("$\\log\\left(N_{\\Delta t}(t)\\right)$") #n(t) = count_v - nulleffekt im zeitintervall
+plt.ylabel("$\\log\\left(N_{\\Delta t}(t)\\right)$")
 plt.grid()
 plt.legend()
 plt.savefig("build/c04_silber2_log.pdf")
