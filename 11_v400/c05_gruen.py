0f325c832d527bb3d8a6e7be7ba1acc34923ee51
Clara Sondermann <clara.sondermann@tu-dortmund.de>
2023-04-30 13:48:19 +0200

    v400: auswertung fertig

--- 11_v400/c05_gruen.py
+++ 11_v400/c05_gruen.py
@@ -8,12 +8,15 @@ import scipy.constants as const
 # gruenes Licht
 # 600.0 Linien pro mm
 # zugehörige Gitterkonstante:  1.6666666666666667 μm
+# zugehöriger Winkel:  [18.5] deg
 # λ gruen bei 600.0 Linien =  [5.288410940084869e-07]
 # 300.0 Linien pro mm
 # zugehörige Gitterkonstante:  3.3333333333333335 μm
+# zugehöriger Winkel:  [ 9.5 19.  29.5] deg
 # λ gruen bei 300.0 Linien =  [5.501586862022589e-07 5.426135907619278e-07 5.471372890038524e-07]
 # 100.0 Linien pro mm
 # zugehörige Gitterkonstante:  10.0 μm
+# zugehöriger Winkel:  [ 3.   6.5  9.5 12.5 15.5 19.5 22.5 25.5 29.5] deg
 # λ gruen bei 100.0 Linien =  [5.233595624294384e-07 5.660160688395336e-07 5.501586862022589e-07
 #  5.410990348452572e-07 5.344767521565138e-07 5.563447653896183e-07
 #  5.46690617664414e-07 5.38138871010369e-07 5.471372890038524e-07]
@@ -33,6 +36,7 @@ print("zugehörige Gitterkonstante: ", gitterkonst_600 * 10**6, "μm")
 
 maximum_600, links_600, rechts_600 = np.genfromtxt("messung_5_600_gruen.txt", unpack = True)
 phi_600 = np.deg2rad((np.abs(links_600 + rechts_600))/2)
+print("zugehöriger Winkel: ", phi_600[1:] / np.pi * 180, "deg")
 
 wavelength_600 = gitterkonst_600 * unp.sin(phi_600[1:]) / maximum_600[1:]
 
@@ -49,6 +53,8 @@ print("zugehörige Gitterkonstante: ", gitterkonst_300 * 10**6, "μm")
 
 maximum_300, links_300, rechts_300 = np.genfromtxt("messung_5_300_gruen.txt", unpack = True)
 phi_300 = np.deg2rad((np.abs(links_300 + rechts_300))/2)
+print("zugehöriger Winkel: ", phi_300[1:] / np.pi * 180, "deg")
+
 
 wavelength_300 = gitterkonst_300 * unp.sin(phi_300[1:]) / maximum_300[1:]
 
@@ -64,6 +70,8 @@ print("zugehörige Gitterkonstante: ", gitterkonst_100 * 10**6, "μm")
 
 maximum_100, links_100, rechts_100 = np.genfromtxt("messung_5_100_gruen.txt", unpack = True)
 phi_100 = np.deg2rad((np.abs(links_100 + rechts_100))/2)
+print("zugehöriger Winkel: ", phi_100[1:] / np.pi * 180, "deg")
+
 
 wavelength_100 = gitterkonst_100 * unp.sin(phi_100[1:]) / maximum_100[1:]
 
--- 11_v400/c05_rot.py
+++ 11_v400/c05_rot.py
@@ -8,12 +8,15 @@ import scipy.constants as const
 # rotes Licht
 # 600.0 Linien pro mm
 # zugehörige Gitterkonstante:  1.6666666666666667 μm
+# zugehöriger Winkel:  [22.5] deg
 # λ rot bei 600.0 Linien =  [6.37805720608483e-07]
 # 300.0 Linien pro mm
 # zugehörige Gitterkonstante:  3.3333333333333335 μm
+# zugehöriger Winkel:  [11.5 23.  36. ] deg
 # λ rot bei 300.0 Linien =  [6.645597813906573e-07 6.51218547482123e-07 6.530947247694146e-07]
 # 100.0 Linien pro mm
 # zugehörige Gitterkonstante:  10.0 μm
+# zugehöriger Winkel:  [ 3.5  7.5 11.5 15.  19.  23.5 27.5] deg
 # λ rot bei 100.0 Linien =  [6.104853953485688e-07 6.526309611002579e-07 6.645597813906573e-07
 #  6.470476127563019e-07 6.511363089143134e-07 6.645817815420771e-07
 #  6.596408760500485e-07]
@@ -33,6 +36,8 @@ print("zugehörige Gitterkonstante: ", gitterkonst_600 * 10**6, "μm")
 
 maximum_600, links_600, rechts_600 = np.genfromtxt("messung_5_600_rot.txt", unpack = True)
 phi_600 = np.deg2rad((np.abs(links_600 + rechts_600))/2)
+print("zugehöriger Winkel: ", phi_600[1:] / np.pi * 180, "deg")
+
 
 wavelength_600 = gitterkonst_600 * unp.sin(phi_600[1:]) / maximum_600[1:]
 
@@ -49,6 +54,8 @@ print("zugehörige Gitterkonstante: ", gitterkonst_300 * 10**6, "μm")
 
 maximum_300, links_300, rechts_300 = np.genfromtxt("messung_5_300_rot.txt", unpack = True)
 phi_300 = np.deg2rad((np.abs(links_300 + rechts_300))/2)
+print("zugehöriger Winkel: ", phi_300[1:] / np.pi * 180, "deg")
+
 
 wavelength_300 = gitterkonst_300 * unp.sin(phi_300[1:]) / maximum_300[1:]
 
@@ -64,6 +71,7 @@ print("zugehörige Gitterkonstante: ", gitterkonst_100 * 10**6, "μm")
 
 maximum_100, links_100, rechts_100 = np.genfromtxt("messung_5_100_rot.txt", unpack = True)
 phi_100 = np.deg2rad((np.abs(links_100 + rechts_100))/2)
+print("zugehöriger Winkel: ", phi_100[1:] / np.pi * 180, "deg")
 
 wavelength_100 = gitterkonst_100 * unp.sin(phi_100[1:]) / maximum_100[1:]
 
